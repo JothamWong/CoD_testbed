4dd6ee187cc502716d5eb6378fb9a2ae4ae26f44
JothamWong <45916998+JothamWong@users.noreply.github.com>
2024-03-13 17:52:41 +0800

    Initial working dump

---
+++ src/main/java/CoD/DataStreamJob.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package CoD;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        // Sets up the execution environment, which is the main entry point
+        // to building Flink applications.
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<String> text = env.socketTextStream("localhost", 9000, "\n");
+        text.
+                flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+                    @Override
+                    public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) {
+                        for (String word : value.split("\\s")) {
+                            collector.collect(Tuple2.of(word, 1));
+                        }
+                    }
+                }).keyBy(new KeySelector<Tuple2<String, Integer>, Object>() {
+                    @Override
+                    public Object getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                        return stringIntegerTuple2.f1;
+                    }
+                })
+                .sum(1)
+                .print();
+        // Execute program, beginning computation.
+        env.execute("Socket WordCount");
+    }
+}
